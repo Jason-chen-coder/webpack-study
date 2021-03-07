df2f0fad329e465c20b271d7cdf57892dbb64fbc
节省钱 <561864073@qq.com>
2021-03-07 22:21:35 +0800

    '打包打包'

--- webpack.config.js
+++ webpack.config.js
@@ -30,51 +30,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          ...commCssLoader
-        ],
-      },
-      {
-        test: /\.less$/,
-        use: [
-          ...commCssLoader,
-          'less-loader',
-        ],
-      },
-      {
-        test: /\.(gif|img|png|jpg)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 8 * 1024,
-          name: '[hash:10].[ext]',
-          esModule: false,
-          outputPath: 'images',
-        },
-      },
-      {
-        // 处理 html 中 img 资源
-        test: /\.(html|htm)$/i,
-        use: 'html-withimg-loader', // 解析 html中的图片资源
-      },
-      {
-        exclude: /\.(css|js|html|less|gif|img|png|jpg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[hash:10].[ext]',
-          outputPath: 'assets',
-        },
-      },
-      /*
-          语法检查： eslint-loader eslint
-          注意：只检查自己写的源代码，第三方的库是不用检查的
-          设置检查规则： package.json 中 eslintConfig 中设置~
-          "eslintConfig": {
-            "extends": "airbnb-base"
-          }
-          airbnb 需要的语法依赖--> eslint-config-airbnb-base eslint-plugin-import eslint
-      */
-      {
+        /*
+            语法检查： eslint-loader eslint
+            注意：只检查自己写的源代码，第三方的库是不用检查的
+            设置检查规则： package.json 中 eslintConfig 中设置~
+            "eslintConfig": {
+              "extends": "airbnb-base"
+            }
+            airbnb 需要的语法依赖--> eslint-config-airbnb-base eslint-plugin-import eslint
+        */
         test: /\.js$/,
         // 不需要语法检查的路径
         exclude: /node_modules/,
@@ -88,42 +52,82 @@ module.exports = {
           fix: true,
         },
       },
-      /*
-       js兼容性处理：需要下载 babel-loader @babel/core
-         1. 基本js兼容性处理 --> @babel/preset-env
-           问题：只能转换基本语法，如promise高级语法不能转换
-         2. 全部js兼容性处理 --> @babel/polyfill
-           问题：只要解决部分兼容性问题，但是将所有兼容性代码全部引入，体积太大了
-         3. 需要做兼容性处理的就做：按需加载  --> core-js
-       最終方案：@babel/preset-env + core-js
-     */
       {
-        test: /\.js$/,
-        exclude: /node_module/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [
-              // 基本预设
-              '@babel/preset-env',
-              {
-                // 按需加载
-                useBuiltIns: 'usage',
-                // 指定core-js版本
-                corejs: { version: 3 },
-                // 指定兼容到什么版本的浏览器
-                targets: {
-                  chrome: '60',
-                  firefox: '50',
-                  ie: '9',
-                  safari: '10',
-                  edge: '17',
-                },
-              },
+        oneOf: [
+          {
+            test: /\.css$/,
+            use: [
+              ...commCssLoader
             ],
-          ],
-        },
-      },
+          },
+          {
+            test: /\.less$/,
+            use: [
+              ...commCssLoader,
+              'less-loader',
+            ],
+          },
+          {
+            test: /\.(gif|img|png|jpg)$/,
+            loader: 'url-loader',
+            options: {
+              limit: 8 * 1024,
+              name: '[hash:10].[ext]',
+              esModule: false,
+              outputPath: 'images',
+            },
+          },
+          {
+            // 处理 html 中 img 资源
+            test: /\.(html|htm)$/i,
+            use: 'html-withimg-loader', // 解析 html中的图片资源
+          },
+          {
+            exclude: /\.(css|js|html|less|gif|img|png|jpg)$/,
+            loader: 'file-loader',
+            options: {
+              name: '[hash:10].[ext]',
+              outputPath: 'assets',
+            },
+          },
+          /*
+           js兼容性处理：需要下载 babel-loader @babel/core
+             1. 基本js兼容性处理 --> @babel/preset-env
+               问题：只能转换基本语法，如promise高级语法不能转换
+             2. 全部js兼容性处理 --> @babel/polyfill
+               问题：只要解决部分兼容性问题，但是将所有兼容性代码全部引入，体积太大了
+             3. 需要做兼容性处理的就做：按需加载  --> core-js
+           最終方案：@babel/preset-env + core-js
+         */
+          {
+            test: /\.js$/,
+            exclude: /node_module/,
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  // 基本预设
+                  '@babel/preset-env',
+                  {
+                    // 按需加载
+                    useBuiltIns: 'usage',
+                    // 指定core-js版本
+                    corejs: { version: 3 },
+                    // 指定兼容到什么版本的浏览器
+                    targets: {
+                      chrome: '60',
+                      firefox: '50',
+                      ie: '9',
+                      safari: '10',
+                      edge: '17',
+                    },
+                  },
+                ],
+              ],
+            },
+          },
+        ]
+      }
     ],
   },
   plugins: [
@@ -140,6 +144,7 @@ module.exports = {
     // 压缩css
     new OptimizeCssAssetsWebpackPlugin(),
   ],
+  devtool: 'eval-source-map',
   // 生产环境(production)下会压缩js文件
   mode: 'development',
   // 实现热更新
